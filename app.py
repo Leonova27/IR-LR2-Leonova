dc03444b65d8efc0a5af7d535ba3f272afe76f56
Leonova27 <nata.leonova27@yandex.ru>
2026-09-19 15:35:58 +1000

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(_name_)
 @app.route("/")
 def hello_world():
     return "Hello, World!"
-if_name_=="_main_":
+if __name__ == "__main__":
 app.run(host="0.0.0.0", port=8080)
 @app.route("/about")
 def about():
